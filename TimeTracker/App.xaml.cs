b2c527d792d8a1f3d697f835d7400a4bcdf28371
garretfick <garret@ficksworkshop.com>
2014-06-23 14:02:48 +0800

    Basic WPF application showing in the task area. The application doesn't do anything yet.

---
+++ TimeTracker/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace Ficksworkshop.TimeTracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon notificationIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+           base.OnStartup(e);
+
+            notificationIcon = (TaskbarIcon)FindResource("NotificationIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notificationIcon.Dispose();
+
+            base.OnExit(e);
+        }
+
+    }
+}

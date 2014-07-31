ec04d8f80ec6f43b942ab2ecabddedbde162dbb6
garretfick <garret@ficksworkshop.com>
2014-07-31 14:13:50 +0800

    Check in initial state for adding commands

--- TimeTrackerCommands/MainWindow.xaml.cs
+++ TimeTrackerCommands/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using Ficksworkshop.TimeTrackerAPI;
 
 namespace Ficksworkshop.TimeTracker
---
+++ TimeTrackerCommands/TimeTrackerViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Ficksworkshop.TimeTrackerAPI;
+
+namespace Ficksworkshop.TimeTracker
+{
+    public class TimeTrackerViewModel : ViewModelBase
+    {
+        private readonly IProjectTimesData _dataSet;
+
+        public ObservableCollection<IProject> Projects { get; private set; }
+
+        public string NewProjectName { get; set; }
+
+        private IProject _selectedProject;
+
+        public IProject SelectedProject
+        {
+            get
+            {
+                return _selectedProject;
+            }
+            set
+            {
+                if (_selectedProject != value)
+                {
+                    _selectedProject = value;
+                    NotifyPropertyChanged("SelectedProject");
+
+                    UpdateStatus();
+                }
+            }
+        }
+
+        private string _status;
+
+        public string Status
+        {
+            get
+            {
+                return _status;
+            }
+            private set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    NotifyPropertyChanged("Status");
+                }
+            }
+        }
+
+        public TimeTrackerViewModel(IProjectTimesData dataSet)
+        {
+            _dataSet = dataSet;
+            _dataSet.ProjectsChanged += DataSetOnProjectsChanged;
+
+            Projects = new ObservableCollection<IProject>();
+        }
+
+        public void AddClicked()
+        {
+            string newName = NewProjectName;
+            _dataSet.CreateProject("", newName);
+        }
+
+        private void DataSetOnProjectsChanged(object sender, object e)
+        {
+            Projects.Clear();
+            foreach (IProject project in _dataSet.Projects)
+            {
+                Projects.Add(project);
+            }
+        }
+
+        private void UpdateStatus()
+        {
+            IProjectTime time = (SelectedProject != null) ? _dataSet.FirstOpenTime(SelectedProject) : null;
+            string status = (time != null) ? time.Start.ToString() : "<None>";
+
+            Status = status;
+        }
+
+        public void PunchInOutClicked()
+        {
+            // Get the selected project
+            if (SelectedProject != null)
+            {
+                IProjectTime time = _dataSet.FirstOpenTime(SelectedProject);
+                if (time == null)
+                {
+                    // Start a new time
+                    _dataSet.CreateTime(SelectedProject, DateTime.Now, null);
+                }
+                else
+                {
+                    // End the current time
+                    time.End = DateTime.Now;
+                }
+
+                UpdateStatus();
+            }
+        }
+    }
+}

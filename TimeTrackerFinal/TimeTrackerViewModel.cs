87e423213916e3f88829f52abacd89a51a0b7f60
garretfick <garret@ficksworkshop.com>
2014-07-31 15:38:30 +0800

    Comments for the final version

--- TimeTrackerFinal/TimeTrackerViewModel.cs
+++ TimeTrackerFinal/TimeTrackerViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
-using System.Windows.Navigation;
-
 using Ficksworkshop.TimeTrackerAPI;
 using Ficksworkshop.TimeTrackerAPI.Commands;
 
@@ -12,13 +9,23 @@ namespace Ficksworkshop.TimeTracker
     {
         private readonly IProjectTimesData _dataSet;
 
+        /// <summary>
+        /// All known projects that we are going to show.
+        /// </summary>
         public ObservableCollection<IProject> Projects { get; private set; }
         
-
+        /// <summary>
+        /// If creating a new project, this is the name of the new project. Don't need
+        /// to implement NotifyPropertyChanged here because it is only set by the view.
+        /// </summary>
         public string NewProjectName { get; set; }
 
+        
         private IProject _selectedProject;
 
+        /// <summary>
+        /// The single currently selected project from <see cref="Projects"/>.
+        /// </summary>
         public IProject SelectedProject
         {
             get
@@ -39,6 +46,11 @@ namespace Ficksworkshop.TimeTracker
 
         private string _status;
 
+        /// <summary>
+        /// The status information for the currently selected project. We could take
+        /// this futher by using a value converter, but that's outside the scope of this
+        /// demonstration.
+        /// </summary>
         public string Status
         {
             get
@@ -55,10 +67,20 @@ namespace Ficksworkshop.TimeTracker
             }
         }
 
+        /// <summary>
+        /// Bindable command to create a new project.
+        /// </summary>
         public ICommand CreateProjectCommand { get; private set; }
 
+        /// <summary>
+        /// Bindable command to punch in/out of the selected project.
+        /// </summary>
         public ICommand PunchInOutCommand { get; private set; }
 
+        /// <summary>
+        /// Initalizes a new instance of the <see cref="TimeTrackerViewModel"/> class.
+        /// </summary>
+        /// <param name="dataSet">The data to adapt.</param>
         public TimeTrackerViewModel(IProjectTimesData dataSet)
         {
             _dataSet = dataSet;
@@ -71,6 +93,11 @@ namespace Ficksworkshop.TimeTracker
             PunchInOutCommand = new PunchInOutCommand(() => _dataSet, () => SelectedProject);
         }
 
+        /// <summary>
+        /// Listens to the ProjectsChanged event from the model so we can notify the view.
+        /// </summary>
+        /// <param name="sender">Default parameter</param>
+        /// <param name="e">Information about the project that was changed.</param>
         private void DataSetOnProjectsChanged(object sender, object e)
         {
             Projects.Clear();
@@ -80,11 +107,19 @@ namespace Ficksworkshop.TimeTracker
             }
         }
 
+        /// <summary>
+        /// Listens to the ProjectTimeChanged event from the model so we can notify the view.
+        /// </summary>
+        /// <param name="sender">Default parameter</param>
+        /// <param name="e">Information about the time that was changed.</param>
         private void DataSetOnTimesChanged(object sender, TimesChangedEventArgs e)
         {
             UpdateStatus();
         }
 
+        /// <summary>
+        /// Helper function to calculate status information. This really should be using a value converter.
+        /// </summary>
         private void UpdateStatus()
         {
             IProjectTime time = (SelectedProject != null) ? _dataSet.FirstOpenTime(SelectedProject) : null;
